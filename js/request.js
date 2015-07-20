938cd22b27631ae257e6edd75a34bf900a292b2f
Wes Hoover <work@weshoover.com>
2015-07-20 17:40:07 -0500

    added event listener and button to search api

--- js/request.js
+++ js/request.js
@@ -1,4 +1,5 @@
-$(function() { //doc ready
+$(document).ready(function() { //doc ready
+
 
   $('#search').on(click, function(event){
     event.preventDefault();
@@ -7,14 +8,16 @@ $(function() { //doc ready
 
 
 
-  function getAsteroids() {
-
+  function getAsteroids(query, limit) { // Query Asterank API
+    var query, limit;
     var earl= "http://asterank.com/api/asterank?query={query}&limit={limit}";
 
     $.ajax(earl,{
       data: '',
+      success: function(data){
 
-
+      },
+      error: ''
     });
   }
 )};
